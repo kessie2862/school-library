813f9c70399bea1e7753abeb35ad11eb427eca46
kessie <kayp3477@gmail.com>
2023-08-02 10:14:53 +0000

    Create Book class

---
+++ book.rb
@@ -0,0 +1,18 @@
+class Book
+  attr_accessor :title, :author, :rentals
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
+
+  def add_rental(rental)
+    @rentals << rental
+    rental.book = self
+  end
+
+  def rented_by
+    rentals.map(&:person)
+  end
+end
